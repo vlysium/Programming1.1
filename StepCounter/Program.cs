ebc9bf47489f84df47f0d904edebb2ee37c97f92
Victor Ly <vict4222@stud.kea.dk>
2026-02-12 12:51:47 +0100

    Add project files.

---
+++ StepCounter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StepCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many steps have you walked today?");
+            int steps = Convert.ToInt16(Console.ReadLine());
+
+            double distancePerStep = 0.762; // in meters
+
+            double distanceWalked = Math.Round((distancePerStep * steps) / 1000, 2);
+
+            Console.WriteLine($"You have walked {distanceWalked} km today");
+        }
+    }
+}
