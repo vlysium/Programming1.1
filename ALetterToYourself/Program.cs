e7276fdfd85968b4a5f082a5278c0f05fc9dcbf1
Victor Ly <vict4222@stud.kea.dk>
2026-02-12 12:49:25 +0100

    Add project files.

---
+++ ALetterToYourself/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ALetterToYourself
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*-------------------------------*");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("|  " + DateTime.Now.ToString("dd-MM-yyyy") + "                   |");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("|  Copenhagen, Denmark          |");
+            Console.WriteLine("|  vil007@edu.zealand.dk        |");
+            Console.WriteLine("|                               |");
+            Console.WriteLine("*-------------------------------*");
+        }
+    }
+}
