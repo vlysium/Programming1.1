798dfc1eb5febb63b1eccb251bd58dc04a52916f
Victor Ly <ly.victor2000@gmail.com>
2026-02-27 10:16:43 +0100

    Yearbook exercise

---
+++ ClassesAndObjects/Yearbook/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace Yearbook {
+
+    public class Person
+    {
+        public string Name { get; }
+
+        public int Age { get; }
+
+        public DateTime Birthday { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the Person class with the specified name, age, and birthday.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the person.
+        /// </param>
+        /// <param name="age">
+        /// The age of the person.
+        /// </param>
+        /// <param name="birthday">
+        /// The birthday of the person.
+        /// </param>
+        public Person(string name, int age, DateTime birthday)
+        {
+            Name = name;
+            Age = age;
+            Birthday = birthday;
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current object, including the name,
+        /// age, and birthday of the person.
+        /// </summary>
+        /// <returns>A string representation of the person.</returns>
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Birthday: {Birthday.ToShortDateString()}";
+        }
+    }
+}
---
+++ ClassesAndObjects/Yearbook/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Yearbook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("John Doe", 30, new DateTime(1994, 5, 15));
+            Person person2 = new Person("Jane Jane", 25, new DateTime(1999, 2, 22));
+
+            Console.WriteLine(person1.ToString());
+            Console.WriteLine(person2.ToString());
+        }
+    }
+}
