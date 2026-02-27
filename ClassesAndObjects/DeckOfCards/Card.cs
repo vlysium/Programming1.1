ce1e158ae157ef9b0ea777785c33dfb4182945b0
Victor Ly <ly.victor2000@gmail.com>
2026-02-27 09:18:17 +0100

    DeckOfCards exercise

---
+++ ClassesAndObjects/DeckOfCards/Card.cs
@@ -0,0 +1,41 @@
+﻿namespace DeckOfCards;
+
+public enum Value { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+
+public enum Suit { Diamonds, Clubs, Hearts, Spades }
+public class Card
+{
+    /// <summary>
+    /// Gets the value of the card (e.g., Ace, Two, etc.).
+    /// </summary>
+    public Value Value { get; }
+    
+    /// <summary>
+    /// Gets the suit of the card (e.g., Diamonds, Clubs, etc.).
+    /// </summary>
+    public Suit Suit { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Card"/> class with the specified value and suit.
+    /// </summary>
+    /// <param name="value">
+    /// The value of the card (e.g., Ace, Two, etc.).
+    /// </param>
+    /// <param name="suit">
+    /// The suit of the card (e.g., Diamonds, Clubs, etc.).
+    /// </param>
+    public Card(Value value, Suit suit)
+    {
+        Value = value;
+        Suit = suit;
+    }
+
+    /// <summary>
+    /// Returns the string representation of the card.
+    /// </summary>
+    /// <returns>A string in the format "<see cref="Value"/> of <see cref="Suit"/>".</returns>
+    public override string ToString()
+    {
+        return $"{Value} of {Suit}";
+    }
+}
---
+++ ClassesAndObjects/DeckOfCards/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DeckOfCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an array to hold 52 cards in the deck
+            Card[] deck = new Card[52];
+
+            // get all possible suits and values from the enums
+            Suit[] suits = (Suit[])Enum.GetValues(typeof(Suit));
+            Value[] values = (Value[])Enum.GetValues(typeof(Value));
+
+            // populate the deck with cards by iterating through each suit and value
+            int index = 0;
+            foreach (Suit suit in suits)
+            {
+                foreach (Value value in values)
+                {
+                    deck[index] = new Card(value, suit);
+                    index++;
+                }
+            }
+            
+            // display all the cards
+            foreach (Card card in deck)
+            {
+                Console.WriteLine(card);
+            }
+        }
+    }
+}
